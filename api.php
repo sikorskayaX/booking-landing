4468630ff1d0320b26ec1538931aabfbfaf2f06b
sikorskayaX <sikorskaya02@gmail.com>
2023-11-12 19:43:19 +0300

    Add form submission handling with jQuery and API endpoint
    This commit adds:
    
    - An API endpoint (api.php) to receive form data
    - jQuery and JavaScript (app.js) to handle form submission and POST data to API
    - Names added to form inputs to associate with JavaScript
    
    This allows capturing submitted form data and passing it to the backend API for processing.

---
+++ api.php
@@ -0,0 +1,7 @@
+﻿<?php 
+    $count = $_GET['count'];
+    $name = $_GET['name'];
+
+    echo '<h1>Количество: ' . $count . '</h1>';
+    echo '<h1>Имя: ' . $name . '</h1>';
+
---
+++ app.js
@@ -0,0 +1,15 @@
+﻿
+$(function(){
+    
+    $('.form-button').on('click', function(){
+        alert('Yes');
+        let data = {
+            'name' : $('[name = "name"]').val(),
+            'phone' : $('[name = "phone"]').val(),
+            'email' : $('[name = "email"]').val()
+        };
+
+        console.log(data);
+        return false;
+    });
+});
